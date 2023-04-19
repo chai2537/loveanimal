4460690b1d21d1332803fc8285ea87f3717a39ed
chai2537 <121778215+chai2537@users.noreply.github.com>
2023-04-19 22:21:38 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = (https://www.love.khao.today/2023/04/18/be-watch-citizens-the-brutal-battle-of-mongoose-vs-cobra-which-ends-in-misery/v) => {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
